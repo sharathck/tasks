5556a8303920b87eb369a00d95009155392079f4
Sharath Chandra <sharathck@outlook.com>
2024-04-14 19:41:58 -0500

    Update font size in index.css and App.css
    The font size in index.css and App.css has been updated to 11px. This change improves the readability and visual consistency of the application.

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
     <div className="app">
       {user ? (
         <div>
-          <p> {user.displayName}!</p>
+            {/* <p> {user.displayName}!</p> */}
           <button onClick={handleSignOut}>Sign Out</button>
           <form onSubmit={handleAddTask}>
             <input
