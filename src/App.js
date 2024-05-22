a90fb3d0fd8538482187200b11d335520543f5ac
Aarush <aarush@gmail.com>
2024-05-22 13:18:42 -0500

    Update CSS files for width, font size, and file paths

--- src/App.js
+++ src/App.js
@@ -182,7 +182,7 @@ function App() {
     };
     
   return (
-    <div className="app">
+    <div className="app" style={{fontSize: '24px' }}>
       {user ? (
         <div>
             {/* <p> {user.displayName}!</p> */}
