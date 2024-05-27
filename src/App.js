ec63816888d2ddb4176447035809b3ddc41ccc08
Sharath Chandra <sharathck@outlook.com>
2024-05-27 02:36:29 -0500

    Update JS file paths and CSS styles for improved performance and usability

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import * as speechsdk from 'microsoft-cognitiveservices-speech-sdk';
 const speechKey = process.env.REACT_APP_AZURE_SPEECH_API_KEY;
 const serviceRegion = 'eastus';
 const voiceName = 'en-US-AvaNeural';
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyBNeonGTfBV2QhXxkufPueC-gQLCrcsB08",
   authDomain: "reviewtext-ad5c6.firebaseapp.com",
   databaseURL: "https://reviewtext-ad5c6.firebaseio.com",
@@ -21,6 +21,16 @@ const firebaseConfig = {
   storageBucket: "reviewtext-ad5c6.appspot.com",
   messagingSenderId: "892085575649",
   appId: "1:892085575649:web:b57abe0e1438f10dc6fca0"
+};*/
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
