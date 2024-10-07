d7b01255ab4608bfe059a28f136e0760abdd9568
Sharath <sharathck@outlook.com>
2024-10-07 13:12:49 -0500

    Update default limit value in TTSQueueApp and replace main.js version in asset manifest and index.html

--- src/TTSQueueApp.js
+++ src/TTSQueueApp.js
@@ -55,7 +55,7 @@ function TTSQueueApp() {
             uid = user.uid;
             const urlParams = new URLSearchParams(window.location.search);
             const limitParam = urlParams.get('limit');
-            const limitValue = limitParam ? parseInt(limitParam) : 11;
+            const limitValue = limitParam ? parseInt(limitParam) : 5;
             //print limit value
             console.log('limit value: ', limitValue);
             const q = query(todoCollection, where('userId', '==', user.uid), where('status', '==', false), orderBy('createdDate', 'desc'), limit(limitValue));
