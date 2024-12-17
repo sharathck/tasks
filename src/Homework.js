e260e980bce26d32f0141dfdd72f6d814c6c6618
Sharath <sharathck@outlook.com>
2024-12-16 19:39:20 -0600

    Update asset manifest and index.html for new main.js file; enhance Homework component with user answer feedback

--- src/Homework.js
+++ src/Homework.js
@@ -346,9 +346,10 @@ const Homework = ({sourceDocumentID}) => {
                 {problems.map((problem, index) => (
                     <div key={index} className="grid-row">
                         <div className="question-col">{problem.question}</div>
-                        {showAnswers && (
-                            <div className="answer-col">{problem.correctAnswer}</div>
-                        )}
+                        {showAnswers && problem.userAnswer && 
+                            <div className="answer-col">{problem.correctAnswer}</div>}
+                        {showAnswers && !problem.userAnswer && 
+                            <div className="answer-col">Complete your answer</div>}
                         <div className="user-answer-col">
                             <input
                                 type="text"
